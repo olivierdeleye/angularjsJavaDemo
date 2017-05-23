b7d9b766dbd39930eae8d0e1a7beca50a930aec7
olivier.deleye <olivier.deleye@gmail.com>
2017-05-23 16:55:54 +0200

    modalService voor leverancier details OK

--- src/main/java/ngdemo/domain/Leverancier.java
+++ src/main/java/ngdemo/domain/Leverancier.java
@@ -7,17 +7,13 @@ package ngdemo.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
-import java.util.Set;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 /**
  *
