841c61c1221de0e0c23f66f915ee0e5d1aa81131
olivier.deleye <olivier.deleye@gmail.com>
2017-05-23 15:46:46 +0200

    modalService voor leverancier details OK

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('ngdemo', ['ngRoute', 'ngTouch', 'ui.grid', 'ngdemo.filters', 'ngdemo.services', 'ngdemo.directives', 'ngdemo.controllers']).
+angular.module('ngdemo', ['ngRoute', 'ngTouch', 'ui.grid', 'ui.grid.selection', 'angularModalService', 'ngdemo.filters', 'ngdemo.services', 'ngdemo.directives', 'ngdemo.controllers']).
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/leveranciers', {templateUrl: 'partials/leveranciers.html', controller: 'leverancierCtrl'});
         $routeProvider.when('/home', {templateUrl: 'partials/welkom.html', controller: 'welkomCtrl'});
--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -16,21 +16,25 @@ var app = angular.module('ngdemo.controllers', []);
 //});
 
 
-app.controller('leverancierCtrl', ['$scope', 'LeverancierResource', function ($scope, LeverancierResource) {
-   
-              
+app.controller('leverancierCtrl', ['$scope', '$log', 'LeverancierResource' ,'ModalService', function ($scope, $log, LeverancierResource, ModalService) {
+        
    $scope.leveranciers = [];
    
-   $scope.gridOptions = { data: 'leveranciers',
-              rowHeight: 25,
-              multiSelect:false,
-              enableRowSelection: true,
-              columnDefs: [
+   $scope.gridOptions = { 
+        enableRowSelection: true,
+        selectionRowHeaderWidth: 35,
+        rowHeight: 35,
+        multiSelect: false,
+        enableGridMenu: true
+    };
+    
+    
+    var gridCols= [
                {
                  
                 field: "leverancierNr",
                 displayName: "Nummer",
-                width: '5%'            
+                width: '15%'            
                 },
                {
                  field: "lev_naam",
@@ -41,7 +45,7 @@ app.controller('leverancierCtrl', ['$scope', 'LeverancierResource', function ($s
                 {
                  field: "groep.groepNaam",
                  displayName: "Groep",
-                 width: '20%'
+                 width: '10%'
                               
                },
                {
@@ -52,7 +56,7 @@ app.controller('leverancierCtrl', ['$scope', 'LeverancierResource', function ($s
                {
                  field: "huisNr",
                  displayName: "HuisNr",
-                 width: '5%'
+                 width: '8%'
                },
                {
                  field: "postcode",
@@ -69,22 +73,91 @@ app.controller('leverancierCtrl', ['$scope', 'LeverancierResource', function ($s
                  displayName: "Telefoon"
                 
                }
+     ];
+ 
 
-               ]};
-           
-    $scope.promise = LeverancierResource.query().$promise.then(function(result){
-             $scope.leveranciers = angular.copy(result);
-            // $scope.gridOptions = $scope.leveranciers;
-             
-        });
-     
-//         $scope.myData = [{name: "Moroni", age: 50},
-//                     {name: "Tiancum", age: 43},
-//                     {name: "Jacob", age: 27},
-//                     {name: "Nephi", age: 29},
-//                     {name: "Enos", age: 34}];
+    $scope.promise = LeverancierResource.query().$promise.then(function(results){
+        $scope.gridOptions.columnDefs = gridCols;
+        $scope.gridOptions.data = results;
+                 
+    });
 
+     $scope.gridOptions.onRegisterApi = function(gridApi){
+      //set gridApi on scope
+      $scope.gridApi = gridApi;
+      
+      gridApi.selection.on.rowSelectionChanged($scope,function(row){
+        var msg = 'row selected ' + row.isSelected;
+        $scope.selectedRow= {};
+        $scope.selectedRow = $scope.gridApi.selection.getSelectedRows();
+        $log.log(msg);
+        $scope.showModal()
+        $log.log($scope.selectedRow[0].postcode);
+      });
+      
+    
+
+      gridApi.selection.on.rowSelectionChangedBatch($scope,function(rows){
+        var msg = 'rows changed ' + rows.length;
+        $log.log(msg);
+      });
+    };
+    
+    $scope.showModal = function() {
+
+            ModalService.showModal({
+              templateUrl: "modals/leverancierModal.html",
+              controller: "leverancierModalCtrl",
+              preClose: (modal) => { modal.element.modal('hide'); },
+              inputs: {
+                title: "Details leverancier",
+                leverancier : $scope.selectedRow[0]
+              }
+            }).then(function(modal) {
+              modal.element.modal();
+              modal.close.then(function(result) {
+                if (!result) {
+                  $scope.complexResult = "Modal forcibly closed..."
+                } else {
+                  $scope.complexResult  = "Name: " + result.name + ", age: " + result.age;
+                }
+              });
+            });
+
+    };
    
+}]);
+
+
+app.controller('leverancierModalCtrl', ['$scope', '$element', 'title', 'leverancier', 'close', 
+  function($scope, $element, title, leverancier, close) {
+
+        $scope.leverancier = leverancier;
+        $scope.title = title;
+
+        //  This close function doesn't need to use jQuery or bootstrap, because
+        //  the button has the 'data-dismiss' attribute.
+        $scope.close = function() {
+                close({
+            name: $scope.name,
+            age: $scope.age
+          }, 500); // close, but give 500ms for bootstrap to animate
+        };
+
+        //  This cancel function must use the bootstrap, 'modal' function because
+        //  the doesn't have the 'data-dismiss' attribute.
+        $scope.cancel = function() {
+
+          //  Manually hide the modal.
+          $element.modal('hide');
+
+          //  Now call close, returning control to the caller.
+          close({
+            name: $scope.name,
+            age: $scope.age
+          }, 500); // close, but give 500ms for bootstrap to animate
+        };
+
 }]);
 
 
